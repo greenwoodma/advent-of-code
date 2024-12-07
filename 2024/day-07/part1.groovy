1bf4c1b83cdb3b1de58960b2fe41197209da261c
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2024-12-07 20:34:59 +0000

    day 7; took ages longer than it should as I had >= not >

---
+++ 2024/day-07/part1.groovy
@@ -0,0 +1,23 @@
+long total = 0;
+
+new File("input.txt").eachLine { line ->
+	data = line.split("[^0-9]+")*.toLong()
+
+	if (check(data[0],data[1],data.tail().tail()))
+		total += data[0]
+}
+
+println total
+
+def boolean check(total, current, rest) {
+	
+	if (rest.size() == 0) return total == current;
+	
+	// took ages to solve as I originally had this as >=
+	// but that's stupid as you could have many more 1s
+	// in the rest and multiplying would keep the same
+	// total
+	if (current > total) return false;
+		
+	return check(total,current+rest[0],rest.tail()) || check(total,current*rest[0],rest.tail())
+}
