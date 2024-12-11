ecf9d6e3f9a72d4d36dc5598d55d3d2a85b54574
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2024-12-11 08:41:41 +0000

    day 11: it's always pleasing when my solution to part1 also works unaltered for part2

---
+++ 2024/day-11/part2.groovy
@@ -0,0 +1,33 @@
+import groovy.transform.Memoized
+
+def stones = (((new File("input.txt")).readLines()[0])).split("\s+")*.toLong()
+
+long total = 0
+
+for (stone : stones) {
+	total += count(stone,75)
+}
+
+println "\n"+total
+
+@Memoized
+def long count(Long stone, int blinks) {
+	if (blinks == 0) {
+		return 1;
+	}
+	
+	if (stone == 0)
+		return count(1,blinks-1)
+		
+	if (stone.toString().length()%2 == 0) {
+		String s = stone.toString();
+
+		Long s1 = Long.valueOf(s.substring(0,(int)(s.length()/2)))
+		Long s2 = Long.valueOf(s.substring((int)(s.length()/2)))
+
+		return count(s1,blinks-1)+count(s2,blinks-1)
+		
+	}
+	
+	return  count(stone*2024,blinks-1)
+}
