3235eb846553431c2f3c2342fc3e98fd72c6e885
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2025-12-03 10:20:06 +0000

    some code comments so anyone looking at it doesn't have to read my mind

--- 2025/day-03/part1.groovy
+++ 2025/day-03/part1.groovy
@@ -3,17 +3,24 @@ List<String> banks = (new File("input.txt")).readLines()
 long total = 0;
 
 for (String bank : banks) {
+
+	// convert the string into an array of ints
 	int[] batteries = Arrays.stream(bank.toCharArray() as String[]).mapToInt(Integer::parseInt).toArray();
-	//System.out.println("\n"+batteries)
+	
+	// get the max number, ignoring the last digit
 	(d1, p1) = findMax(batteries,0, batteries.length-1);
+	
+	// get the max number after the number we found above
 	(d2, p2) = findMax(batteries,p1+1, batteries.length);
 	
-	//System.out.println(d1+""+d2)
+	// make the number by adding the two digits consecutively
 	total += Long.valueOf(d1+""+d2)
 }
 
 System.out.println(total)
 
+// returns the max digit (and it's position) between two
+// offsets into the array
 def findMax(int[] batteries, int start, int end) {
 	int max = -1;
 	int pos = -1;
--- 2025/day-03/part2.groovy
+++ 2025/day-03/part2.groovy
@@ -3,31 +3,47 @@ List<String> banks = (new File("input.txt")).readLines()
 long total = 0;
 
 for (String bank : banks) {
+
+	// convert the string into an array of ints
 	int[] batteries = Arrays.stream(bank.toCharArray() as String[]).mapToInt(Integer::parseInt).toArray();
 		
+	// we are looking for a sequence of twelve so start by not looking
+	// at the last 11 batteries
 	int end = batteries.length - 11;
+	
+	// but we can look from the beginning of the bank
 	int start = 0;
 	
+	// this is the 12 digit number we are building
 	String n = "";
 	
-	
-	
+	// for each of the twelve digits
 	for (int p = 0 ; p < 12 ; ++p) {
+		
+		// find biggest digit in the current valid range
 		(d1, next) = findMax(batteries, start, end);
 		
+		// add the digit to the end of the number
 		n = n+""+d1;
-		++end;
+		
+		// next time start from the digit after the one we
+		// have just found...
 		start = next+1
+		
+		// ... but we can look at one more digit at the end
+		// of the battery bank
+		++end;
+		
 	}
-	
-	
 
-	//System.out.println(d1+""+d2)
+	// add the number to the total
 	total += Long.valueOf(n)
 }
 
 System.out.println(total)
 
+// returns the max digit (and it's position) between two
+// offsets into the array
 def findMax(int[] batteries, int start, int end) {
 	int max = -1;
 	int pos = -1;
